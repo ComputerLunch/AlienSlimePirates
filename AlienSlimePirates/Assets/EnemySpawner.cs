6365278c3354f8f9ddbbcddeb99f12ca6c06ac91
DESKTOP-VJ5QSEB\Yiyi <yiyiwzhang@gmail.com>
2017-09-27 19:42:02 -0400

    Create enemy ship spawner, set random spawn intervals

---
+++ AlienSlimePirates/Assets/EnemySpawner.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemySpawner : MonoBehaviour {
+
+	public static int enemyCount;
+	public static int maxEnemy;
+	public GameObject enemySlime;
+
+	// Use this for initialization
+		
+	void Start () {
+		enemyCount = 0;
+		maxEnemy = 10;
+		Invoke ("SpawnEnemy", 1f);
+	}
+
+	void SpawnEnemy(){
+
+		if (enemyCount < maxEnemy) {
+			Instantiate (enemySlime, gameObject.transform.position, Quaternion.identity);
+			enemyCount++;
+			Invoke ("SpawnEnemy", Random.Range(1f,5f));
+		}
+	}
+
+}
