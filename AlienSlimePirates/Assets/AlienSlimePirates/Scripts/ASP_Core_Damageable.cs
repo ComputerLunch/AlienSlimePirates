057220103ea258ebfa2d5a54ea365d8f7b9ae3dc
Michael de la Peña <mdlp@dreamscience.com>
2017-10-02 17:31:32 -0400

    Wrote Core script and applied to modified core prefab "Core_mdlp"

---
+++ AlienSlimePirates/Assets/AlienSlimePirates/Scripts/ASP_Core_Damageable.cs
@@ -0,0 +1,40 @@
+﻿
+using UnityEngine;
+using UnityEngine.UI;
+
+// ASP_Core_Damageable is a specialized version of ASP_Damageable just for the core
+public class ASP_Core_Damageable : ASP_Damageable
+{
+
+    //Text output of current damage
+    [SerializeField]
+    private ASP_PercentAnimator damagePercentText;
+ [SerializeField]
+    private ASP_InnerCylinderAnimator innerCylinder;
+
+    protected override void Start()
+    {
+        //call base method of ASP_Core_Damageable
+        base.Start();
+        damagePercentText.UpdatePercent( 100);
+    }
+
+
+    protected override void CauseDamage(int damage)
+    {
+        damageReceived += damage;
+        if (damageReceived >= hitPoints)
+        {
+			//destroyed
+           	innerCylinder.UpdateValue(0);
+            damagePercentText.UpdatePercent (0);
+			ASP_GameManager.Instance.GameOver();
+        }
+        else
+        {
+			float newPercentage = 1-((float)damageReceived / (float)hitPoints);
+			innerCylinder.UpdateValue(newPercentage);
+            damagePercentText.UpdatePercent (System.Convert.ToInt32( newPercentage*100));
+        }
+    }
+}
---
+++ AlienSlimePirates/Assets/AlienSlimePirates/Scripts/ASP_InnerCylinderAnimator.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ASP_InnerCylinderAnimator : MonoBehaviour
+{
+
+    // Use this for initialization
+
+
+
+    private float currentValue = 1;
+    private float targetValue = 1;
+    [SerializeField]
+    private float ScaleAmountPerTick = 0.01f;
+
+    //
+    // Update is called once per frame
+    public void UpdateValue(float newNumber)
+    {
+        targetValue = newNumber;
+   
+    }
+    void Update()
+    {
+
+        if (currentValue > targetValue)
+        {
+            currentValue -= ScaleAmountPerTick * Time.deltaTime;
+         
+            if (currentValue > 0)
+            {
+                transform.localScale = new Vector3(1, currentValue, 1);
+            }
+            else
+            {
+                currentValue = 0;
+                targetValue = 0;
+                transform.localScale = new Vector3(1, 0, 1);
+            }
+
+        }
+
+
+    }
+
+}
