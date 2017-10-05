127751fd8038d2a43ac836dcbd8b588f73fc181c
DESKTOP-VJ5QSEB\Yiyi <yiyiwzhang@gmail.com>
2017-10-04 20:20:58 -0400

    Fix propeller lift so that it comes down as well as goes up

--- AlienSlimePirates/Assets/VRTK/Examples/ExampleResources/Scripts/HandLift.cs
+++ AlienSlimePirates/Assets/VRTK/Examples/ExampleResources/Scripts/HandLift.cs
@@ -9,45 +9,60 @@
         public Transform handleTop;
         public Transform ropeTop;
         public Transform ropeBottom;
+		public Vector3 startPosition;
         public GameObject rope;
         public GameObject handle;
 
         private bool isMoving = false;
         private bool isMovingUp = true;
+		private bool isGrabbed = false;
+
+		void Start (){
+			startPosition = handle.transform.position;
+		}
 
         public override void OnInteractableObjectGrabbed(InteractableObjectEventArgs e)
         {
-            base.OnInteractableObjectGrabbed(e);
 
             isMoving = true;
+			isGrabbed = true;
+
+			base.OnInteractableObjectGrabbed(e);
         }
+
+		public override void OnInteractableObjectUngrabbed(InteractableObjectEventArgs e)
+		{
+			isGrabbed = false;
+			base.OnInteractableObjectUngrabbed(e);
+		}
 
         protected override void Update()
         {
             base.Update();
 
-            if (isMoving)
-            {
-                Vector3 movePosition = (isMovingUp ? Vector3.up : Vector3.down) * speed * Time.deltaTime;
+			if (isMoving) {
+				Vector3 movePosition = (isMovingUp ? Vector3.up : Vector3.down) * speed * Time.deltaTime;
 
-                handle.transform.position += movePosition;
+				handle.transform.position += movePosition;
 
-                Vector3 scale = rope.transform.localScale;
-                scale.y = (ropeTop.position.y - handle.transform.position.y) / 2.0f;
+				Vector3 scale = rope.transform.localScale;
+				scale.y = (ropeTop.position.y - handle.transform.position.y) / 2.0f;
 
-                Vector3 midpoint = ropeTop.transform.position;
-                midpoint.y -= scale.y;
+				Vector3 midpoint = ropeTop.transform.position;
+				midpoint.y -= scale.y;
 
-                rope.transform.localScale = scale;
-                rope.transform.position = midpoint;
+				rope.transform.localScale = scale;
+				rope.transform.position = midpoint;
 
-                if((!isMovingUp && handle.transform.position.y <= ropeBottom.position.y) || 
-                    (isMovingUp && handle.transform.position.y >= handleTop.position.y))
-                {
-                    isMoving = false;
-                    isMovingUp = !isMovingUp;
-                }
-            }
+				if ((!isMovingUp && handle.transform.position.y <= ropeBottom.position.y) || (isMovingUp && handle.transform.position.y >= handleTop.position.y)) {
+					isMoving = false;
+					isMovingUp = !isMovingUp;
+				} 
+			}
+			else if (isGrabbed == false && isMoving == false)
+			{
+				handle.transform.position = startPosition;
+			}
         }
     }
 }
