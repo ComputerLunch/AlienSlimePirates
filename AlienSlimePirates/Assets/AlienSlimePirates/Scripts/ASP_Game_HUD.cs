3ca78a52288fb4460abebb65cf4b9885910f6e87
garrahan\Andrew <andrew@gutpela.com>
2017-10-06 17:19:43 -0400

    Added lightsaber pedestal Moved gun to crows nest Partly implemented health bar in HUD

--- AlienSlimePirates/Assets/AlienSlimePirates/Scripts/ASP_Game_HUD.cs
+++ AlienSlimePirates/Assets/AlienSlimePirates/Scripts/ASP_Game_HUD.cs
@@ -9,11 +9,20 @@ public class ASP_Game_HUD : MonoBehaviour
     private Text currentScoreLabel;
     [SerializeField]
     private Text currentScoreOutput;
+	[SerializeField]
+	private Image playerDamageImage;
+	[SerializeField]
+	private Image playerHealthImage;
+	[SerializeField]
+	private float playerDamage_zero;
+	[SerializeField]
+	private float playerDamage_Max;
+
+
     [SerializeField]
     private Text gameOver;
     [SerializeField]
     private Text victory;
-
     [SerializeField]
     private Text defeat;
     [SerializeField]
@@ -25,8 +34,14 @@ public class ASP_Game_HUD : MonoBehaviour
     // Use this for initialization
     void Start()
     {
+		//During game HUD
         currentScoreLabel.enabled = true;
         currentScoreOutput.enabled = true;
+		playerHealthImage.gameObject.SetActive (true);
+		playerDamageImage.gameObject.SetActive (true);
+		playerDamageImage.transform.position = new Vector3 (playerDamageImage.transform.position.x, playerDamage_zero, playerDamageImage.transform.position.z);
+
+		//Post game HUD
         gameOver.enabled = false;
         victory.enabled = false;
         defeat.enabled = false;
@@ -34,7 +49,13 @@ public class ASP_Game_HUD : MonoBehaviour
         FinalScoreLabel.enabled = false;
         FinalScoreOutput.enabled = false;
     }
-
+	public void PlayerDamaged(float damagePercent){
+		float difference = playerDamage_zero - playerDamage_Max;
+		float newY = playerDamage_zero + difference * damagePercent;
+		print ("newY " + newY);
+		playerDamageImage.transform.position =  new Vector3 (playerDamageImage.transform.position.x,newY, playerDamageImage.transform.position.z);
+			
+	}
 
     public void SetNewScore(string newScore)
     {
@@ -44,7 +65,7 @@ public class ASP_Game_HUD : MonoBehaviour
     public void DisplayFinalResults(GameResult result, string finalScore)
     {
 
-		print ("*******************************FOO *************************** " + finalScore);
+
         currentScoreLabel.enabled = false;
         currentScoreOutput.enabled = false;
 
