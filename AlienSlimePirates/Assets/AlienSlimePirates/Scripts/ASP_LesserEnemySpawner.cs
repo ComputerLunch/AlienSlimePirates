3479f23973a3ff72b2bbbda9676a48b5f192e823
DESKTOP-VJ5QSEB\Yiyi <yiyiwzhang@gmail.com>
2017-10-20 16:39:59 -0400

    Lesser enemy AI, bullet spawning, targeting player

---
+++ AlienSlimePirates/Assets/AlienSlimePirates/Scripts/ASP_LesserEnemySpawner.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ASP_LesserEnemySpawner : MonoBehaviour {
+	public  float enemySpawnDelay = 25 ;
+	public  int enemyCount = 0 ;
+	public  int maxEnemy = 10;
+	public GameObject enemySlime;
+
+	// Use this for initialization
+		
+	void Start () {
+		//enemyCount = 0;
+		//maxEnemy = 10;
+		Invoke ("SpawnEnemy", enemySpawnDelay);
+		ASP_GameManager.Instance.RegisterLevelEnemies(maxEnemy);
+	}
+
+	void SpawnEnemy(){
+
+		if (enemyCount < maxEnemy) {
+			Instantiate (enemySlime, gameObject.transform.position, Quaternion.identity);
+			enemyCount++;
+			Invoke ("SpawnEnemy", Random.Range(10f,25f));
+		}
+	}
+
+}
---
+++ AlienSlimePirates/Assets/AlienSlimePirates/Scripts/LesserEnemyBehavior.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class LesserEnemyBehavior: MonoBehaviour {
+
+	public Transform targetCore;
+	public Transform targetPlayer;
+	public float speed;
+
+	public GameObject enemyBullet;
+	public Rigidbody enemyBulletRB;
+	public Transform enemyShotSpawn;
+	public float enemyFireRate;
+	public float enemyBulletSpeed;
+
+	private float playerDist;
+	private float coreDist;
+	private float enemyNextFire;
+
+	private NavMeshAgent agent;
+
+	void Start()
+	{
+		agent = gameObject.GetComponent<NavMeshAgent> ();
+	}
+
+	void Update() {
+
+		float step = speed * Time.deltaTime;
+
+		targetPlayer = GameObject.FindWithTag ("Player").transform; //position of player
+		targetCore = GameObject.FindWithTag ("Core").transform; //position of core
+
+		playerDist = Vector3.Distance (targetPlayer.position, transform.position);
+		coreDist = Vector3.Distance (targetCore.position, transform.position);
+
+		if (playerDist > 25f || coreDist > 25f) {
+			//agent.SetDestination (targetPlayer.position);
+			transform.position = Vector3.MoveTowards (transform.position, targetPlayer.position, step);
+		}
+
+		if (Time.time > enemyNextFire) {
+			
+			enemyNextFire = Time.time + enemyFireRate;
+			GameObject bullet = Instantiate (enemyBullet, enemyShotSpawn.position, enemyShotSpawn.rotation);
+			enemyBulletRB = bullet.GetComponent<Rigidbody> ();
+			enemyBulletRB.AddForce ((targetCore.position - transform.position).normalized * enemyBulletSpeed);
+
+		}
+
+
+	}
+}
